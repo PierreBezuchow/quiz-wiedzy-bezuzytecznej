3f81fdc660d133d71153ce691107a7deb3a0372f
PierreBezuchow <axel@starosta.pl>
2018-02-22 17:01:22 +0100

    Przechodzenie do następnych pytań, zbieranie odpowiedzi prawidłowych i od użytkownika

--- src/js/Quiz.js
+++ src/js/Quiz.js
@@ -27,47 +27,82 @@ module.exports = [
         c: "dlatego, że było to miejsce największej ilości misji kościelnych na świecie ",
         d: "dlatego, że występowały tam jedyne na świecie kamienie szlachetne o czarnym zabarwieniu (onyks",
         correctAnswer: "a"
-    }
-    // {
-    //     question: "",
-    //     answers: {
-    //         a: "",
-    //         b: "",
-    //         c: "",
-    //         d: ""
-    //     },
-    //     correctAnswer: "d"
-    // },
-    // {
-    //     question: "",
-    //     answers: {
-    //         a: "",
-    //         b: "",
-    //         c: "",
-    //         d: ""
-    //     },
-    //     correctAnswer: "d"
-    // },
-    // {
-    //     question: "",
-    //     answers: {
-    //         a: "",
-    //         b: "",
-    //         c: "",
-    //         d: ""
-    //     },
-    //     correctAnswer: "d"
-    // },
-    // {
-    //     question: "",
-    //     answers: {
-    //         a: "",
-    //         b: "",
-    //         c: "",
-    //         d: ""
-    //     },
-    //     correctAnswer: "d"
-    // },
+    },
+    {
+        number: 4,
+        question: "Sean Connery zagrał w „Indiana Jones i ostatnia krucjata” ojca Indiany. O ile lat w rzeczywistości Connery jest starszy od Forda?",
+            a: "5",
+            b: "9",
+            c: "12",
+            d: "15",
+        correctAnswer: "c"
+    },
+    {
+        number: 5,
+        question: "Hiszpania ma najwięcej (pod względem powierzchni) winnic na świecie. Ale kto jest na drugim miejscu?",
+
+            a: "Francja",
+            b: "Chiny",
+            c: "Argentyna",
+            d: "RPA",
+        correctAnswer: "b"
+    },
+    {
+        number: 6,
+        question: "Pod jakim tytułem wszedł do polskich kin w 1984 roku film „Terminator” z Arnoldem Schwarzeneggerem?",
+            a: "Elektroniczny morderca",
+            b: "Cyborg z przyszłości",
+            c: "Robot zabójca",
+            d: "Rycerz przyszłości",
+        correctAnswer: "d"
+    },
+    {
+        number: 7,
+        question: "Truskawka to tak naprawdę:",
+        a: "gatunek jagody",
+        b: "warzywo",
+        c: "mieszanka poziomek",
+        d: "gatunek ananasa",
+        correctAnswer: "c"
+    },
+    {
+        number: 8,
+        question: "O tym, że klacz marszałka Piłsudskiego miała na imię Kasztanka wie wielu z nas. Ale jak miał na imię pies marszałka – wilczur niemiecki?",
+        a: "Pies",
+        b: "Burek",
+        c: "Legion",
+        d: "Czarny",
+        correctAnswer: "a"
+    },
+    {
+        number: 9,
+        question: "Mieszkaniec miejscowości Swornegacie to:",
+        a: "swornegacczanin",
+        b: "swornegacianin",
+        c: "swornianin",
+        d: "sworniańczyk",
+        correctAnswer: "b"
+    },
+    {
+        number: 10,
+        question: "Dopiero teraz, 70 lat po wojnie, liczba ludzi narodowości żydowskiej na świecie osiągnęła poziom niemal taki, jaki miała w przededniu wybuchu II wojny światowej. W którym kraju w Europie przed wojną mieszkało najwięcej Żydów?",
+        a: "W Polsce",
+        b: "W ZSRR",
+        c: "W Rumunii",
+        d: "W Niemczech",
+        correctAnswer: "a"
+    },
+    {
+        number: 11,
+        question: "Honorowy Obywatel Stanów Zjednoczonych– określenie osoby, która otrzymała honorowe obywatelstwo amerykańskie. Honorowe obywatelstwo amerykańskie nadawane jest obywatelom innych państw ze względu na szczególne zasługi dla USA. \n" +
+        "Decyzję o jego nadaniu podejmuje bezpośrednio Kongres Stanów Zjednoczonych, lub prezydent, za uprzednim upoważnieniem ze strony Kongresu. \n" +
+        "Obywatelstwo to przyznano do tej pory 8 osobom. Czy są wśród nich Polacy?",
+        a: "Niestety nie ma",
+        b: "Tak, ale jest tylko jeden",
+        c: "połowa z nich to Polacy (czyli 4)",
+        d: "większość z nich to polacy (czyli >4)",
+        correctAnswer: "b"
+    },
     // {
     //     question: "",
     //     answers: {
--- src/js/Quiz.jsx
+++ src/js/Quiz.jsx
@@ -4,38 +4,84 @@ import ReactDOM from 'react-dom';
 // import myQuestions from './Questions.js';
 import questions from './Quiz.js';
 
+class Result extends React.Component{
+        constructor(props){
+            super(props);
+            this.state={
+            userAnswers: this.props.userAnswers,
+            correctAnswers: this.props.correctAnswers
+            };
+        }
+
+
+        
+
+    render(){
+        console.log(this.props.correctAnswers);
+        console.log(this.state.userAnswers);
+
+        return (
+            <div>Twój wynik to </div>
+        );
+    }
+}
+
 class Question extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            question: this.props.questions,
             selectedAnswer: '',
-            number: 1
+            answer: '',
+            correctAnswer: this.props.correctAnswer,
+            score: 0,
+
 
         };
+        this.handleChange = this.handleChange.bind(this);
+
     }
 
+
+
+
+    handleChange(event) {
+        this.setState({
+            selectedAnswer: event.target.value,
+            answer: event.target.value,
+        });
+        this.props.setAnswer(event.target.value);
+        this.props.pushAnswer(event.target.value);
+        this.props.correctAnswers(this.state.correctAnswer);
+        console.log(event.target.value);
+        console.log(this.state.score);
+
+    }
+
+
+
     render() {
+
+
         return <div className='question'>
-            <h1>Pytanie nr {this.props.number}</h1>
+            <h1>Pytanie nr {this.props.id + 1}</h1>
             <h2>{this.props.question}</h2>
             <label>
-                <h3><input onChange={this.props.handleChange} type="radio" value="a"
+                <h3><input onChange={this.handleChange} id='radioButton' type="radio" value="a"
                        checked={this.state.selectedAnswer === 'a'}/>
                 A: {this.props.a}</h3>
             </label>
             <label>
-                <h3><input onChange={this.props.handleChange} type="radio" value="b"
+                <h3><input onChange={this.handleChange} id='radioButton' type="radio" value="b"
                        checked={this.state.selectedAnswer === 'b'}/>
                 B: {this.props.b}</h3>
             </label>
             <label>
-                <h3><input onChange={this.props.handleChange} type="radio" value="c"
+                <h3><input onChange={this.handleChange} id='radioButton' type="radio" value="c"
                        checked={this.state.selectedAnswer === 'c'}/>
                 C: {this.props.c}</h3>
             </label>
             <label>
-                <h3><input onChange={this.props.handleChange} type="radio" value="d"
+                <h3><input onChange={this.handleChange} id='radioButton' type="radio" value="d"
                        checked={this.state.selectedAnswer === 'd'}/>
                     D: {this.props.d}</h3>
             </label>
@@ -48,69 +94,98 @@ class Questions extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            question: this.props.questions,
+            id: 0,
+            correctAnswer: '',
+            answer: '',
+            userAnswers: [],
             selectedAnswer: '',
-            number: 1
+            score: 0,
+            correctAnswers: [],
+
+
 
         };
-        this.handleChange = this.handleChange.bind(this);
+        this.nextQuestion = this.nextQuestion.bind(this);
+        this.setAnswer = this.setAnswer.bind(this);
+        this.pushAnswer = this.pushAnswer.bind(this);
+        this.correctAnswers = this.correctAnswers.bind(this);
+
+
+
     }
 
-    nextQuestion() {
+    correctAnswers(value){
         this.setState({
-            number: number + 1
+            correctAnswer: value
         });
-        console.log('clk');
-
-
-
     }
 
-    handleChange(event) {
+    pushAnswer(value){
         this.setState({
-            selectedAnswer: event.target.value
+            answer: value
         });
-        console.log(event.target.value);
     }
+
+    setAnswer(value){
+        this.setState({
+            selectedAnswer: value
+        });
+    }
+
+    nextQuestion(){
+        this.setState({
+            correctAnswers: [...this.state.correctAnswers, this.state.correctAnswer],
+            userAnswers: [...this.state.userAnswers, this.state.answer],
+            id: this.state.id + 1,
+
+
+        });
+        this.setState({
+            selectedAnswer: ''
+        });
+
+    }
+
+
+
 
     render() {
 
         const question = this.props.questions.map((element, index) => {
-            return (<Question key={index} question={element.question} a={element.a} b={element.b}
-                              c={element.c} d={element.d} number={element.number} handleChange={this.handleChange}/>
-                // <div className='question'>
-                //     <h1>Pytanie nr {element.number}</h1>
-                //     <h2>{element.question}</h2>
-                //     <h3>a: {element.a}</h3>
-                //     <h3>b: {element.b}</h3>
-                //     <h3>c: {element.c}</h3>
-                //     <h3>d: {element.d}</h3>
-                // </div>
+            return (<div>
+                <Question key={index} question={element.question} a={element.a} b={element.b}
+                              c={element.c} d={element.d} number={element.number} id={index} correctAnswer={element.correctAnswer} setAnswer={this.setAnswer}
+                              pushAnswer={this.pushAnswer} correctAnswers={this.correctAnswers}
+                              />
+                </div>
+
             );
         });
+        if(this.state.id === this.props.questions.length - 1) {
+            return (
+                <div>
+                    <Result userAnswers={this.state.userAnswers} correctAnswers={this.state.correctAnswers}/>
+                </div>
+            );
+        }
 
-        return (
-            <div>
+            return (
+                <div>
+                    {question[this.state.id]}
+                    <button type="submit" onClick={this.nextQuestion}
+                            value='Next'
+                            disabled={this.state.selectedAnswer.length === 0}>{this.state.id === this.props.questions.length - 2 ? 'Zakończ i pokaż wyniki' : 'Następne pytanie'}</button>
+                </div>
 
-                {question[1]}
+            );
+        }
 
-                        <button type="submit" onClick={this.nextQuestion} value="Next">Następne</button>
-                    </div>
 
-        );
-    }
 }
 
 
 export default class Quiz extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            questions: this.props.questions,
-            correctAnswer: '',
-            userAnswers: []
-        };
-    }
+
 
 
 
@@ -120,6 +195,7 @@ export default class Quiz extends React.Component {
         return (
             <div>
                 <Questions questions={questions}/>
+
             </div>
         );
     };
